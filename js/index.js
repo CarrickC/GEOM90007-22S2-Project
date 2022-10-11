de5b59a8b0859166097b2d2c484c9c9560aff590
Carrick <chcvictory@163.com>
2022-10-11 19:35:44 +1100

    Add javascript folder

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// document.location.hash = '#tab-bus';
+document.location.hash = '#tab-ov';
+
+let tabs = document.querySelectorAll('.navbar-tab')
+for (let i=0; i<tabs.length; i++) {
+    // tabs[i].setAttribute('data-index', i.toString());
+    tabs[i].onclick = function() {
+        for (let i=0; i<tabs.length; i++) {
+            tabs[i].classList.remove('current-tab')
+        }
+        this.classList.add('current-tab');
+    }
+}
+
+respondToVisibility = function(element, callback) {
+    let options = {
+        root: document.documentElement
+    }
+    let observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                callback(entry.intersectionRatio > 0);
+            });
+        }, options);
+
+    observer.observe(element);
+}
