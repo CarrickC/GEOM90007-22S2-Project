f7e23f29e7284099db04bea89ed5c8d6fc83f007
Dansssss <danssmonash@gmail.com>
2022-10-20 02:42:17 +1100

    add chart into popup

--- js/tab-rail.js
+++ js/tab-rail.js
@@ -21,7 +21,6 @@ const stations = {
       'properties': {
         'address': 'South Kensington',
         'rail': 'Williamstown, Werribee',
-        'chart': 'chart1'
       }
     },
     {
@@ -33,7 +32,6 @@ const stations = {
       'properties': {
         'address': 'Kensington',
         'rail': 'Craigieburn',
-        'chart': 'chart2'
       }
     },
     {
@@ -45,7 +43,6 @@ const stations = {
       'properties': {
         'address': 'Macaulay',
         'rail': 'Upfield',
-        'chart': 'chart3'
       }
     },
     {
@@ -57,7 +54,6 @@ const stations = {
       'properties': {
         'address': 'Flemington Bridge',
         'rail': 'Upfield',
-        'chart': 'chart4'
       }
     },
     {
@@ -69,7 +65,6 @@ const stations = {
       'properties': {
         'address': 'Royal Park',
         'rail': 'Upfield',
-        'chart': 'chart5'
       }
     },
     {
@@ -81,7 +76,6 @@ const stations = {
       'properties': {
         'address': 'North Melbourne',
         'rail': 'Flemington, Sunbury, Upfield, Werribee, Williamstown, Craigieburn',
-        'chart': 'chart6'
       }
     },
     {
@@ -93,7 +87,6 @@ const stations = {
       'properties': {
         'address': 'Southern Cross',
         'rail': 'Belgrave, Craigieburn, Cranbourne, Flemington, Frankston, Glen Waverley, Hurstbridge, Lilydale, Mernda, Pakenham, Sandringham, Sunbury, Upfield, Werribee, Williamstown, Alamein',
-        'chart': 'chart7'
       }
     },
     {
@@ -105,7 +98,6 @@ const stations = {
       'properties': {
         'address': 'Flagstaff',
         'rail': 'Belgrave, Craigieburn, Cranbourne, Flemington, Glen Waverley, Hurstbridge, Lilydale, Mernda, Pakenham, Sunbury, Upfield, Alamein',
-        'chart': 'chart8'
       }
     },{
       'type': 'Feature',
@@ -116,7 +108,6 @@ const stations = {
       'properties': {
         'address': 'Melbourne Central',
         'rail': 'Belgrave, Craigieburn, Cranbourne, Flemington, Glen Waverley, Hurstbridge, Lilydale, Mernda, Pakenham, Sunbury, Upfield, Alamein',
-        'chart': 'chart9'
       }
     },{
       'type': 'Feature',
@@ -127,7 +118,6 @@ const stations = {
       'properties': {
         'address': 'Flinders Street',
         'rail': 'Belgrave, Craigieburn, Cranbourne, Flemington, Glen Waverley, Hurstbridge, Lilydale, Mernda, Pakenham, Sunbury, Upfield, Alamein',
-        'chart': 'chart10'
       }
     },{
       'type': 'Feature',
@@ -138,7 +128,6 @@ const stations = {
       'properties': {
         'address': 'Parliament',
         'rail': 'Belgrave, Craigieburn, Cranbourne, Flemington, Glen Waverley, Hurstbridge, Lilydale, Mernda, Pakenham, Sunbury, Upfield, Alamein',
-        'chart': 'chart11'
       }
     },{
       'type': 'Feature',
@@ -149,7 +138,6 @@ const stations = {
       'properties': {
         'address': 'Jolimont',
         'rail': 'Mernda, Hurstbridge',
-        'chart': 'chart12'
       }
     }
   ]
@@ -165,8 +153,6 @@ railMap.addSource('places', {
   });
 
 addMarkers();
-
-
 
 });
 
@@ -189,94 +175,6 @@ function addMarkers() {
       /* Fly to the point */
       flyToStation(marker);
       createPopUp(marker);
-
-      var areaChartOptions = {
-        maintainAspectRatio: false,
-        responsive: true,
-        legend: {display: false},
-        scales: {xAxes: [{gridLines: {display: false,}}],
-                 yAxes: [{gridLines: {display: false,}}]}}
-      var chartOptions = jQuery.extend(true, {}, areaChartOptions)
-
-      var ChartData1 = {
-      		  labels: ['2018', '2019', '2020'],
-      		  datasets: [{
-      		    label: 'flow',
-      		    backgroundColor: 'rgba(60,141,188,0.9)',
-      		    data: [289550, 225150, 101750]
-      		  }, {}]
-      		}
-            var chart1Canvas = $('#chart1').get(0).getContext('2d')
-      		var chart1Data = jQuery.extend(true, {}, ChartData1)
-      		chart1Data.datasets[0].fill = false;
-      		chart1Data.datasets[1].fill = false;
-      		chartOptions.datasetFill = false
-      		var chart1 = new Chart(chart1Canvas, {
-      		  type: 'bar',
-      		  data: chart1Data,
-      		  options: chartOptions
-      		})
-
-      /* Highlight listing in sidebar */
-      const activeItem = document.getElementsByClassName('active');
-      e.stopPropagation();
-      if (activeItem[0]) {
-        activeItem[0].classList.remove('active');
-      }
-      const listing = document.getElementById(
-        `listing-${marker.properties.id}`
-      );
-      //listing.classList.add('active');
-    });
-  }
-}
-
-function addMarkers2() {
-  /* For each feature in the GeoJSON object above: */
-  for (const marker of stations.features) {
-    /* Create a div element for the marker. */
-    const el = document.createElement('div');
-    /* Assign a unique `id` to the marker. */
-    el.id = `marker-${marker.properties.id}`;
-    el.className = 'marker';
-
-    new mapboxgl.Marker(el, {
-        offset: [0, -23]
-      })
-      .setLngLat(marker.geometry.coordinates)
-      .addTo(railMap);
-
-    el.addEventListener('click', (e) => {
-      /* Fly to the point */
-      flyToStation(marker);
-      createPopUp(marker);
-
-      var areaChartOptions = {
-        maintainAspectRatio: false,
-        responsive: true,
-        legend: {display: false},
-        scales: {xAxes: [{gridLines: {display: false,}}],
-                 yAxes: [{gridLines: {display: false,}}]}}
-      var chartOptions = jQuery.extend(true, {}, areaChartOptions)
-
-      var ChartData2 = {
-      		  labels: ['2018', '2019', '2020'],
-      		  datasets: [{
-      		    label: 'flow',
-      		    backgroundColor: 'rgba(60,141,188,0.9)',
-      		    data: [2850, 2250, 8750]
-      		  }, {}]
-      		}
-            var chart2Canvas = $('#chart2').get(0).getContext('2d')
-      		var chart2Data = jQuery.extend(true, {}, ChartData2)
-      		chart2Data.datasets[0].fill = false;
-      		chart2Data.datasets[1].fill = false;
-      		chartOptions.datasetFill = false
-      		var chart2 = new Chart(chart2Canvas, {
-      		  type: 'bar',
-      		  data: chart2Data,
-      		  options: chartOptions
-      		})
 
       /* Highlight listing in sidebar */
       const activeItem = document.getElementsByClassName('active');
@@ -309,22 +207,87 @@ function flyToStation(currentFeature) {
 function createPopUp(currentFeature) {
   const popUps = document.getElementsByClassName('mapboxgl-popup');
   if (popUps[0]) popUps[0].remove();
+
+
   const popup = new mapboxgl.Popup({
       closeOnClick: true
     })
     .setLngLat(currentFeature.geometry.coordinates)
     .setHTML(
       `
-      <h4>${currentFeature.properties.address}</h4><div>${currentFeature.properties.rail}</div>
-      <div>
-      <div class="chart"><canvas id= ${currentFeature.properties.chart} ></canvas></div>
-        </div>`
+      <h4>${currentFeature.properties.address}</h4>
+      <h5>Rail: ${currentFeature.properties.rail}</h5>
+      <h6>city circle station entries 2018 - 2020</h6>
+      <div class="chart"><canvas id= "lineChart"style="min-height: 200px; height: 350px; max-height: 300px; max-width: 100%;" ></canvas></div>`
     )
     .addTo(railMap);
+
+    var areaChartData = {
+    			labels: ['2018', '2019', '2020'],
+    			datasets: [
+    			    {
+                        label: 'Flagstaff',
+                        backgroundColor: 'rgba(29, 72, 35, 0.8)',
+                        data: [4854950, 2882250, 1378000]
+                    },
+                    {
+                        label: 'Flinders Street',
+                        backgroundColor: 'rgba(75, 120, 81, 0.8)',
+                        data: [28320650, 21503500, 8528000]
+                    },
+    				{
+                        label: 'Melbourne Central',
+                        backgroundColor: 'rgba(125, 156, 129, 0.8)',
+                        data: [15250700, 10865600, 4206700]
+                    },
+    				{
+                        label: 'Parliament',
+                        backgroundColor: 'rgba(78, 178, 92, 0.8)',
+                        data: [10124200, 7646300, 2213650]
+                    },
+    				{
+                        label: 'Southern Cross',
+                        backgroundColor: 'rgba(180, 227, 186, 0.8)',
+                        data: [19551450, 14895400, 4556450]
+                     }
+    			]
+    		}
+
+    		//these are the options for testing
+    		var areaChartOptions = {
+    			maintainAspectRatio: false,
+    			responsive: true,
+    			legend: {
+    				display: false
+    			},
+    			scales: {
+    				xAxes: [{
+    					gridLines: {
+    						display: false,
+    					}
+    				}],
+    				yAxes: [{
+    					gridLines: {
+    						display: false,
+    					}
+    				}]
+    			}
+    		}
+
+    		var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+    		var lineChartOptions = jQuery.extend(true, {}, areaChartOptions)
+    		var lineChartData = jQuery.extend(true, {}, areaChartData)
+    		lineChartData.datasets[0].fill = false;
+    		lineChartData.datasets[1].fill = false;
+    		lineChartOptions.datasetFill = false
+
+    		var lineChart = new Chart(lineChartCanvas, {
+    			type: 'line',
+    			data: lineChartData,
+    			options: lineChartOptions
+    		})
+
 }
-
-// popup table
-
 
 Chart.defaults.font.family = 'Montserrat';
 Chart.defaults.font.size = 15;
